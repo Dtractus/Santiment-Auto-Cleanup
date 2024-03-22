1ceb554de62dd18ac8bcd89c040b544d684cece3
Mehmet Kaan KARAGÖZOĞLU <55835876+Dtractus@users.noreply.github.com>
2024-03-22 11:57:33 +0300

    Update DtractusAutoPrune.sh

--- DtractusAutoPrune.sh
+++ DtractusAutoPrune.sh
@@ -7,6 +7,7 @@ read -p "Enter the maximum disk usage percent (e.g., 90): " MAX_DISK_USAGE_PERCE
 # Gereksinimlerin Kurulumu
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip git curl
+pip install docker
 
 # Python script'inin indirilmesi ve ilgili dizine taşınması
 git clone https://github.com/Dtractus/Santiment-Auto-Cleanup.git /$HOME/DtractusAutoPrune
@@ -18,5 +19,5 @@ sed -i "s|DISK_NAME = \"/dev/sda1\"|DISK_NAME = \"${DISK_NAME}\"|g" sanr-cleanup
 sed -i "s/MAX_DISK_USAGE_PERCENT = 90/MAX_DISK_USAGE_PERCENT = ${MAX_DISK_USAGE_PERCENT}/g" sanr-cleanup.py
 
 # Cron job'ın eklenmesi
-(crontab -l 2>/dev/null; echo "0 */6 * * * /$HOME/DtractusAutoPrune/sanr-cleanup.py >> /$HOME/DtractusAutoPrune/DtractusSanrLog.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "0 */6 * * * /usr/bin/python3 /$HOME/DtractusAutoPrune/sanr-cleanup.py >> /$HOME/DtractusAutoPrune/DtractusSanrLog.log 2>&1") | crontab -
 
